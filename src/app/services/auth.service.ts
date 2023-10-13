2b3cfdab4f37c95ee96343834d7e84ec699b465f
Stephanie Hallberg <stephaniiehallberg@gmail.com>
2023-10-13 13:27:19 +0200

    54. authenticated user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,15 @@ export class AuthService {
   constructor( private http: HttpClient) {
    }
 
-   register(data: any): Observable<any> {
-    return this.http.post(`${environment.api}/register`, data)
+   register(data: any): Observable<User> {
+    return this.http.post<User>(`${environment.api}/register`, data)
    }
 
    login(data: any): Observable<any> {
     return this.http.post(`${environment.api}/login`, data, {withCredentials: true})
+   }
+
+   user(): Observable<User> {
+    return this.http.get<User>(`${environment.api}/user`, {withCredentials: true});
    }
 }
